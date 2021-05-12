c8b708b737ec302f4492148484f8da99f96f3f71
Bruno Cayres Messias <brunocmessias@hotmail.com>
2021-05-12 18:09:14 -0300

    Adding project

---
+++ Project/compressor.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+--Entity
+entity compressor is port
+(
+	X : in  std_logic;
+	S : out std_logic
+);
+end entity;
+
+
+-- Architecture
+architecture logic of compressor is 
+-- Signals
+
+
+--Components
+
+begin
+
+end architecture;
