20733aa2736f9f986d92eb3b2ff0836e5fb64391
Felipe Hugo <fehugo.costa@hotmail.com>
2021-05-12 19:15:39 -0300

    compressores_adicionados

---
+++ Project/FA22_3.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.Std_Logic_1164.all;
+
+
+ENTITY FA22_3 IS
+PORT(
+		A0,A1,B0,B1: IN STD_LOGIC;
+		S0,S1,COUT0: OUT STD_LOGIC
+	  );
+END FA223;
+
+ARCHITECTURE SOMA22_3 OF FA22_3 IS
+SIGNAL CS0: STD_LOGIC;
+ COMPONENT FA is
+		port (A: in std_logic;
+				B: in std_logic;
+				Cin: in std_logic;
+				S: out std_logic;
+				Cout: out std_logic
+);
+end COMPONENT;
+
+COMPONENT HA IS
+port (
+	A: in std_logic;
+	B: in std_logic;
+	S: out std_logic;
+	Cout: out std_logic
+);
+end COMPONENT;
+
+BEGIN 
+FA1: HA PORT MAP (A0,B0,S0,CS0);
+FA2: FA PORT MAP (A1,B1,CS0,S1,COUT0);
+END SOMA22_3;
---
+++ Project/FA333_33.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.Std_Logic_1164.all;
+
+ENTITY FA333_33 IS
+PORT(
+		A0,A1,A2,B0,B1,B2,C0,C1,C2 : IN STD_LOGIC;
+		S0,S1,S2,COUT0,C0UT1,COUT2: OUT STD_LOGIC
+	  );
+END FA333_33;
+
+ARCHITECTURE SOMA333_33 OF FA333_33 IS
+ 
+ COMPONENT FA is
+		port (A: in std_logic;
+				B: in std_logic;
+				Cin: in std_logic;
+				S: out std_logic;
+				Cout: out std_logic
+);
+end COMPONENT; 
+
+
+BEGIN 
+FA1: FA PORT MAP (A0,B0,C0,S0,COUT0);
+FA2: FA PORT MAP (A1,B1,C1,S1,C0UT1);
+FA3: FA PORT MAP (A2,B2,C2,S2,COUT2);
+
+END SOMA333_33;
